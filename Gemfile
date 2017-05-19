2cdbc210dc8ced723ef8ec3325ed578d036ad16d
m21212c@gmail.com <m21212c@gmail.com>
2017-05-19 11:56:51 -0600

    Babbler and Pry

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+gem "pry" #bug checker
+gem "babbler" #generates random text
---
+++ gems_classes.rb
@@ -0,0 +1,9 @@
+require "pry" #tells compiler that the program uses this gem.
+require "babbler"
+
+puts Babbler.babble
+
+first_name = "first"
+last_name = "last"
+binding.pry #put this where you want to stop execution and to inspect.
+age = "27"
